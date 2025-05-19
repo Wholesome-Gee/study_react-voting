5ae3395341691a23632f671b88d39a0cf9a4c7f8
Wholesome-Gee <jiyong0419@naver.com>
2025-05-19 18:09:38 +0900

    reset css 설정

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,50 @@
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+@import url('https://fonts.googleapis.com/css2?family=Nanum+Gothic&display=swap');
+article, aside, details, figcaption, figure, 
+footer, header, hgroup, menu, nav, section {
+  display: block;
+}
+body {
+  font-family: "Nanum Gothic", sans-serif;
+  font-size: 1rem;
+  line-height: 1;
+}
+ol, ul {
+  list-style: none;
+}
+blockquote, q {
+  quotes: none;
+}
+blockquote:before, blockquote:after,
+q:before, q:after {
+  content: '';
+  content: none;
+}
+table {
+  border-collapse: collapse;
+  border-spacing: 0;
+}
+* {
+  margin: 0;
+  padding: 0;
+  border: 0;
+  font-size: 100%;
+  font: inherit;
+  vertical-align: baseline;
+  box-sizing: border-box;
+}
+a {
+  text-decoration: none;
+  color: inherit;
+}
+ul {
+  list-style-type: none;
+}
+`;
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
