2fad6f40b37cc0f236f1f1d70d21230748fa8af1
Wholesome-Gee <jiyong0419@naver.com>
2025-05-19 18:37:27 +0900

    react-helmet 설치
    Project Tab Title 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
+import { Helmet } from "react-helmet";
+
 function App() {
-  return <div></div>;
+  return (
+    <>
+      <Helmet>
+        <title>VOTING | 소중한 한표, 올바른 투표, 공정한 개표</title>
+      </Helmet>
+    </>
+  );
 }
 
 export default App;
